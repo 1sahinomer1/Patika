b6a9f5a9dea62f728db0e5e93cf81c657608d513
omersahin <1sahinomer1@gmail.com>
2021-04-17 22:34:24 +0300

    todoapp

--- Javascript/Odev2/js/script.js
+++ Javascript/Odev2/js/script.js
@@ -1,25 +1,39 @@
-function myFunction() {
-  let task = document.getElementById("addText").value;
-  if (task !== "") {
-    addTodo(task);
-  } else {
-    alert("task isn't empty");
-  }
-  document.getElementById("addText").value = "";
-}
+"use strict";
+const addButton = document.querySelector(".button");
+var newTask = document.getElementById("task");
+var todoList = document.querySelector("#list");
+var listLi = document.querySelectorAll("#list > li");
+let closeBtn = document.querySelectorAll("span.close");
 
-function addTodo(task) {
-  let ul = document.querySelector("ul");
-  let li = document.createElement("li");
-  li.innerHTML = `
-        <div><p>${task}</p></div>
-        <div>
-        <button type="submit" onclick="clear()">clear</button>
-        <button onclick="check()">check</button>
-        </div>
-    `;
-  ul.appendChild(li);
+function complete(event) {
+  if (event.target.parentElement.parentElement.className === "checked") {
+    event.target.parentElement.parentElement.className = "task-element";
+  } else {
+    event.target.parentElement.parentElement.className = "checked";
+  }
 }
-function clear() {
-  console.log("ssssssss");
+function remove(event) {
+  event.currentTarget.parentElement.remove();
+}
+function newElement() {
+  if (newTask.value.trim(" ")) {
+    let li = document.createElement("li");
+    let ul = document.querySelector("ul");
+    li.innerHTML = `
+            <div>
+                <p>${newTask.value}</p>
+            </div>
+                <span onclick="complete(event)">
+                <i class="fas fa-check"></i>
+                </span>
+                <span class="close" type="submit" onclick="remove(event)">x</span>
+        `;
+    li.className = "task-element";
+    ul.appendChild(li);
+    newTask.value = "";
+    $(".success").toast("show");
+  } else {
+    $(".error").toast("show");
+    newTask.value = "";
+  }
 }
